0902614c39bae9c693807ae6d899933e0cf315bc
Alessandro Pezzè <igougi.ui@gmail.com>
2015-09-30 22:42:35 +0200

    Added ip, and insertion in db

--- client/Mrb.in.js
+++ client/Mrb.in.js
@@ -1,18 +1,17 @@
-Groups = new Mongo.Collection("groups"); //connects to Groups collections
-Meteor.subscribe("groups"); //listen for changes
+PastesLinks = new Mongo.Collection("pastesLinks"); //connection to Group Collection //Do NOT declare as var, it will override the server one
 // counter starts at 0
 Session.setDefault('counter', 0);
 
 Template.body.events({
-  "submit .new-group": function (event) {
-    // Grab group name from text field
-    var newGroup = event.target.group.value;
-    // Check that text field is not blank before adding group
-    if (newGroup !== '') {
-        Meteor.call("addGroup", newGroup); //call server-side method addGroup
+  "submit .new-paste": function (event) {
+    // Grab paste's text from text field
+    var newPaste = event.target.paste.value;
+    // Check that text field is not blank before adding paste
+    if (newPaste !== '') {
+        Meteor.call("addPaste", newPaste); //call server-side method addPaste
     }
     // Clear the text field for next entry
-    event.target.group.value = "";
+    event.target.paste.value = "";
     // Prevent default form submit
     return false;
   }
--- server/Mrb.in.js
+++ server/Mrb.in.js
@@ -1,18 +1,38 @@
-Groups = new Mongo.Collection("groups"); //connection to Group Collection
-Meteor.publish("groups", function () { 
-	return Groups.find(); // everything
+var fs = Npm.require('fs');
+var path = Npm.require( 'path' ) ;
+var __ROOT_APP_PATH__ = fs.realpathSync('.');
+var pastesPath = path.join(__ROOT_APP_PATH__,"pastes");
+
+if(!fs.existsSync(pastesPath)){
+	fs.mkdirSync(pastesPath, 0766, function(err){
+		if(err){ 
+			console.log(err);
+			response.send("ERROR! Can't make the directory! \n");    // echo the result back
+		}
+	});   
+}	
+var PastesLinks = new Mongo.Collection("pastesLinks"); //connection to Group Collection //var=> not sharable
+/* 
+Meteor.publish("pasteLinkss", function () { //share db to the client 
+	return PastesLinks.find(); // everything
 });
+*/
 
 Meteor.methods({ //called by Clients
-	addGroup: function (name) {
-		Groups.insert({
-			name: name,
+	addPaste: function (name) {
+		var filePath = path.join(pastesPath, name + ".txt" );
+		var buffer = new Buffer( name );
+		fs.writeFileSync( filePath, buffer );
+		PastesLinks.insert({
+			link: filePath,
 			createdAt: new Date(),
-			owner: Meteor.uuid(),
-			checked: false
+			owner : Meteor.uuid(),
+			ip : this.connection.clientAddress
 		});
 	}
 });
+
+
 
 Meteor.startup(function () {
 	// code to run on server at startup
